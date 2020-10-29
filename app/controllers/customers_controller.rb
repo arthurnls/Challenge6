bb71f4e7b5afb8e69819d0f2b738e4399ec6c6a1
Arthur Saraiva <arthurnl@gmail.com>
2020-10-28 19:15:55 -0500

    finished part2

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,5 @@
 class CustomersController < ApplicationController
-  before_action :set_customer, only: %i[show edit update destroy]
+  # before_action :set_customer, only: %i[show edit update destroy]
 
   # GET /customers
   # GET /customers.json
@@ -7,69 +7,77 @@ class CustomersController < ApplicationController
     @customers = Customer.all
   end
 
+  def sorted
+    @customers = Customer.order(:name).all
+  end
+
+  def missing_email
+    @customers = Customer.where("email = ''")
+  end
+
   # GET /customers/1
   # GET /customers/1.json
-  def show
-    @customer = Customer.find(params[:id])
-  end
+  # def show
+  #   @customer = Customer.find(params[:id])
+  # end
 
-  # GET /customers/new
-  def new
-    @customer = Customer.new
-  end
+  # # GET /customers/new
+  # def new
+  #   @customer = Customer.new
+  # end
 
-  # GET /customers/1/edit
-  def edit; end
+  # # GET /customers/1/edit
+  # def edit; end
 
-  # POST /customers
-  # POST /customers.json
-  def create
-    @customer = Customer.new(customer_params)
+  # # POST /customers
+  # # POST /customers.json
+  # def create
+  #   @customer = Customer.new(customer_params)
 
-    respond_to do |format|
-      if @customer.save
-        format.html { redirect_to @customer, notice: "Customer was successfully created." }
-        format.json { render :show, status: :created, location: @customer }
-      else
-        format.html { render :new }
-        format.json { render json: @customer.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  #   respond_to do |format|
+  #     if @customer.save
+  #       format.html { redirect_to @customer, notice: "Customer was successfully created." }
+  #       format.json { render :show, status: :created, location: @customer }
+  #     else
+  #       format.html { render :new }
+  #       format.json { render json: @customer.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
-  # PATCH/PUT /customers/1
-  # PATCH/PUT /customers/1.json
-  def update
-    respond_to do |format|
-      if @customer.update(customer_params)
-        format.html { redirect_to @customer, notice: "Customer was successfully updated." }
-        format.json { render :show, status: :ok, location: @customer }
-      else
-        format.html { render :edit }
-        format.json { render json: @customer.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # # PATCH/PUT /customers/1
+  # # PATCH/PUT /customers/1.json
+  # def update
+  #   respond_to do |format|
+  #     if @customer.update(customer_params)
+  #       format.html { redirect_to @customer, notice: "Customer was successfully updated." }
+  #       format.json { render :show, status: :ok, location: @customer }
+  #     else
+  #       format.html { render :edit }
+  #       format.json { render json: @customer.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
-  # DELETE /customers/1
-  # DELETE /customers/1.json
-  def destroy
-    @customer.destroy
-    respond_to do |format|
-      format.html { redirect_to customers_url, notice: "Customer was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
+  # # DELETE /customers/1
+  # # DELETE /customers/1.json
+  # def destroy
+  #   @customer.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to customers_url, notice: "Customer was successfully destroyed." }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
-  private
+  # private
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_customer
-    @customer = Customer.find(params[:id])
-  end
+  # # Use callbacks to share common setup or constraints between actions.
+  # def set_customer
+  #   @customer = Customer.find(params[:id])
+  # end
 
-  # Only allow a list of trusted parameters through.
-  def customer_params
-    params.require(:customer).permit(:name, :phone, :email, :image, :notes)
-  end
+  # # Only allow a list of trusted parameters through.
+  # def customer_params
+  #   params.require(:customer).permit(:name, :phone, :email, :image, :notes)
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :customers
-
   root "customers#index"
+
+  # resources :customers, only: [:index]
+
+  get "/", to: "customers#index"
+  get "/customers/sorted", to: "customers#sorted"
+  get "/customers/missing_email", to: "customers#missing_email"
 end
